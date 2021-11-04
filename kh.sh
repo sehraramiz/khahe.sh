8f08e4520704cd9c6f190f002c0f89be07bdbea8
Mohsen Barzegar <mohsennbarzegar@gmail.com>
2021-11-05 03:14:56 +0330

    New style

--- kh.sh
+++ kh.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-xdotool type $(cat $1 | dmenu -l 20 -i)
+
+taarofsPath="$HOME/scripts/taarofs.txt"
+font="IRTerafik:regular:pixelsize=30"
+xdotool type "$(cat $taarofsPath | dmenu -l 20 -i -fn $font -nb '#000' -nf '#fff456' -sb '#800')"
+
